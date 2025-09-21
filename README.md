a13a65fbd199ddf6aa5f9659c84f3a58db92dd84
cristobers <72763707+cristobers@users.noreply.github.com>
2025-09-21 13:53:18 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# random-name-gen
+
+Generates a random username
+
+## Usage
+
+Create combinations of Adjectives, Nouns and Digits to create random usernames.
+
+```
+A -> Adjective
+N -> Noun
+D -> Digit
+```
+
+```
+stack run "AANDD"
+>>> "terseslackhuman80"
+```
---
+++ app/Main.hs
@@ -0,0 +1,49 @@
+module Main (main) where
+
+import Lib
+import Data.Char
+import System.Random
+import System.Environment
+import Control.Exception
+import Words
+
+type Name = String
+type Nouns = IO [String]
+type Adjectives = IO [String]
+type Input = String 
+
+main :: IO ()
+main = getArgs >>= handleArgs
+
+handleArgs :: [String] -> IO ()
+handleArgs [] = putStrLn "You didn't give a format!\nTry \"AANDD\""
+handleArgs (x:xs) = do
+    res <- sequence [ dsl c (pure nouns) (pure adjectives) | c <- x ]
+    putStrLn $ concat res
+
+dsl :: Char -> Nouns -> Adjectives -> IO String
+dsl 'A' _ adjs     = getRandomEntry adjs
+dsl 'N' nouns _    = getRandomEntry nouns
+dsl 'D' _  _    = do
+    i <- getRandomIndex 10
+    pure (show i :: String)
+dsl _ _ _    = error "Unknown"
+
+getRandomIndex :: Int -> IO Int
+getRandomIndex size = getStdRandom (randomR (0,size-1))
+
+capitalise :: String -> String
+capitalise s = [intoUppercase $ head s] ++ tail s
+ 
+intoUppercase :: Char -> Char
+intoUppercase c 
+    | asNumber > 122 || asNumber < 97 = error "Not a lowercase character."
+    | otherwise                       = chr $ (asNumber) - 32
+    where
+        asNumber = ord c
+
+getRandomEntry :: IO [String] -> IO String
+getRandomEntry xs = do
+    elements <- xs
+    index <- getRandomIndex (length elements)
+    pure $ capitalise $ elements !! index
